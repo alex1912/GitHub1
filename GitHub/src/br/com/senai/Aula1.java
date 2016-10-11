e8d7924145822f55fc953cfd93a04d300088fde1
TeoResende <resendenetto@yahoo.com.br>
2016-10-11 10:50:19 -0300

    Novo método

--- GitHub/src/br/com/senai/Aula1.java
+++ GitHub/src/br/com/senai/Aula1.java
@@ -3,11 +3,17 @@ package br.com.senai;
 import javax.swing.JOptionPane;
 
 public class Aula1 {
+	
+	static String msg = "Bem vindo ao GitHub";
 
 	public static void main(String[] args) {
 		
-		JOptionPane.showMessageDialog(null,"Bem vindo ao GitHub");
+		JOptionPane.showMessageDialog(null,msg);
 
+	}
+	
+	static void trocaMsg(){
+		msg = JOptionPane.showInputDialog("Entre com uma nova mensagem ");
 	}
 
 }
