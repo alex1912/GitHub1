708c447bdeced7ebc92af11b5c14e970cea51565
TeoResende <resendenetto@yahoo.com.br>
2016-10-11 10:23:59 -0300

    Primeiro teste

--- GitHub/src/br/com/senai/Aula1.java
+++ GitHub/src/br/com/senai/Aula1.java
@@ -1,8 +1,12 @@
 package br.com.senai;
 
+import javax.swing.JOptionPane;
+
 public class Aula1 {
 
 	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null,"Bem vindo ao GitHub");
 
 	}
 
